0fb641548c95a4d8d1b828f3461ef37011f6ace2
mp <zlei78511@gmail.com>
2023-08-05 00:13:13 +0800

    精准比例、轻松支持百万级并发
    内置各种算法引擎，提高矿场利润；无感支持最新币种！

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="3.0.0"
+VERSION="3.1.0"
 DOWNLOAD_HOST="https://github.com/minerproxy-btc/RustMinerSystem/raw/main/linux"
 ORIGIN_EXEC="rustminersystem-${VERSION}"
 
@@ -366,6 +366,7 @@ ISSUE() {
     echo "2.0.9"
     echo "2.1.0"
     echo "3.0.0"
+    echo "3.1.0"
 }
 
 filterResult() {
